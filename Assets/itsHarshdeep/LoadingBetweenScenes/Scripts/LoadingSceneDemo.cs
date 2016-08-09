88594666f24961b67d7cad7df9bcc1d92e338062
Inderdeep Singh <sandstargames@gmail.com>
2016-08-10 02:55:18 +0530

    - Version 1 submitted

---
+++ Assets/itsHarshdeep/LoadingBetweenScenes/Scripts/LoadingSceneDemo.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Add following header files
+using ItsHarshdeep.LoadingScene.Constants;
+using ItsHarshdeep.LoadingScene.Controller;
+
+public class LoadingSceneDemo : MonoBehaviour
+{
+	public bool requiredCustomDelay = true;
+
+	void Start ()
+	{
+	}
+
+	// Calling from outside on the 'Load Scene 1' & 'Load Scene 2' button
+	public void LoadScene (string sceneName)
+	{
+		if (requiredCustomDelay)
+			SceneController.LoadLevel (sceneName, 1.5f);
+		else
+			SceneController.LoadLevel (sceneName);
+			
+		print ("Previous Scene name was : " + SceneController.previousScene);
+	} 
+
+	// This will automatically load the previously loaded scene
+	public void LoadPreviousScene ()
+	{
+		SceneController.LoadPreviousScene ();
+
+		/*You may pass te Delay parameter for the previous scene's Loading scene 
+		 *Example : SceneController.LoadPreviousScene(1.25f);
+		 */
+
+	}
+
+}
---
+++ Assets/itsHarshdeep/LoadingBetweenScenes/Scripts/SceneController.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections;
+using ItsHarshdeep.LoadingScene.Constants;
+
+#if (!UNITY_5_2 ||  !UNITY_5_2_3)
+using UnityEngine.SceneManagement;
+#endif
+
+namespace ItsHarshdeep.LoadingScene.Controller
+{
+
+/// <summary>
+/// It will add loading scene when transaction between two scenes
+/// </summary>
+	public class SceneController
+	{
+
+		public static string scene = "";
+		public static string previousScene = ""; 
+
+		/// <summary>
+		/// It will automatically load the "Loading Scene" in-between your requested scene
+		/// </summary>
+		/// <returns>The level.</returns>
+		/// <param name="sceneName">Scene name.</param>
+
+		public static void LoadLevel (string sceneName, float loadingSceneWaitTime = 0)
+		{
+			Constants.Constants.LOADING_SCENE_WAIT_TIME = loadingSceneWaitTime;
+			#if (!UNITY_5_2 ||  !UNITY_5_2_3)
+			previousScene = SceneManager.GetActiveScene ().name;
+			#else
+			previousScene = Application.loadedLevelName.ToString();
+			#endif
+			scene = sceneName;
+			#if (!UNITY_5_2 ||  !UNITY_5_2_3)
+			SceneManager.LoadSceneAsync (Constants.LOADING_SCENE_NAME);
+			#else
+			Application.LoadLevelAsync (Constants.Constants.LOADING_SCENE_NAME);
+			#endif
+		
+		}
+
+		public static void LoadPreviousScene (float loadSceneDelayTime = 0)
+		{
+			if (previousScene == null || previousScene.TrimEnd ().ToString () == "")
+				Debug.LogError ("There is currently no any previous scene yet");
+			else 
+				LoadLevel (previousScene, loadSceneDelayTime);
+		}
+
+
+	}
+}
