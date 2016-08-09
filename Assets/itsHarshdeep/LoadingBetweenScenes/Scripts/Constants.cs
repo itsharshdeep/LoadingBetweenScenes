f105fac7bc916a91b9b0f1cffe16a492cd74afe2
Harshdeep Singh <its.kikex@gmail.com>
2016-08-10 02:58:01 +0530

    - Version 1 Submitted

--- Assets/itsHarshdeep/LoadingBetweenScenes/Scripts/Constants.cs
+++ Assets/itsHarshdeep/LoadingBetweenScenes/Scripts/Constants.cs
@@ -5,7 +5,7 @@ namespace ItsHarshdeep.LoadingScene.Constants
 {
 	public class Constants
 	{
-		public const string LOADING_SCENE_NAME = "LoadingScene";
+		public const string LOADING_SCENE_NAME = "LoadingScene"; 
 		public static float LOADING_SCENE_WAIT_TIME = 1.5f;
 	}
 }
